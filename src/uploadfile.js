7cf00f634e8ca57eb3985e26990a31990bba8886
Tom Becker <tjbecker@me.com>
2019-07-23 17:02:00 +0700

    added upload file to backend

--- src/uploadfile.js
+++ src/uploadfile.js
@@ -7,44 +7,68 @@ class Upload extends Component {
 //state
 
 	state = {
+		// text: '',
 		file: null
 	}
 
 //functions
+changeText = (e) => {
+	this.setState({
+		text: e.target.value
+	})
+}
 
+clearMessage = () => {
+	this.setState({
+		text: ''
+	})
+}
 
-	handleFile(e){
-		let file = e.target.files[0]
-		this.setState({file: file})
+addFile = (e) => {
+	this.setState({
+		file: e.target.files[0]
+	})
+}
+
+createNew = (e, text, file) => {
+		e.preventDefault()
+		// console.log('file', file)
+		let file_holder = new FormData()
+		file_holder.append('file', file)
+		// file_holder.append('name', text)
+		// file_holder.append('channel', this.props.channel)
+		console.log('file_holder', file_holder)
+		let newAudio = {
+			// name: text,
+			file: file_holder,
+			// channel: this.props.channel
+		}
+		console.log('newAudio', newAudio)
+		axios.post(`${process.env.REACT_APP_API}/full`, file_holder).then((res) => {
+				console.log('success');
+		}).catch((err) => {
+			console.log('err', err)
+		})
 	}
-
-	handleUpload(e){
-
-		axios.post(`${process.env.REACT_APP_API}/api/upload`, this.state.file).then((res)=>{
-			console.log('succes');
-		}).catch((err)=> {
-				console.log('err', err);
-			})
-	}
-
-
 
 
 
 //render
 	render() {
 	    return (
-				<div className="container">
-					<div className="row">
-						<div class="offset-md-3 col-md-6">
-							<div class="form-group files">
-								<label>Upload your file</label>
-								<input type="file" name="file" onChange={(e)=>this.handleFile(e)} />
-							</div>
-							<button type="button" class="btn btn-success btn-block" onClick={(e)=>this.handleUpload(e)} >Upload</button>
+				<div id="upload-file">
+				<form onSubmit={(e) => {
+						this.createNew(e, this.state.text, this.state.file); this.clearMessage()}
+					}>
+					<div className="input-group">
+						<input type="file" name="file" onChange={this.addFile}/>
+						<input type="text" className="form-control" placeholder="File Name" value={this.state.text} onChange={(e) => this.changeText(e)} />
+						<div className="input-group-append">
+							<button type="submit" className="btn btn-success">Send</button>
 						</div>
 					</div>
-				</div>
+				</form>
+			</div>
 	    )
 	  }
 	}
