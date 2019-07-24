7c7b733573f7f763d7a8581bbf44911a0a051061
Tom Becker <tjbecker@me.com>
2019-07-24 22:17:41 +0700

    added get analysis to sidebar

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
 import Options from './options'
-
+import axios from 'axios'
 
 class Sidebar extends Component {
 	//data
-
+state = {
+	analysis: []
+}
 	//functions
-
+	componentWillMount() {
+			axios.get('http://localhost:4000/full').then((res)=> {
+				console.log(res.data);
+				this.setState({
+					analysis: res.data
+				})
+			}).catch((err)=> {
+				console.log('err', err);
+			})
+		}
 
 	//render
 	render() {
@@ -14,11 +25,15 @@ class Sidebar extends Component {
 			<div className="col-4">
 		<div className="card">
 			<div className="card-body">
-				<h5 className="card-title">Analysis</h5>
+				<h5 className="card-title">Previous Analysis</h5>
 				<div className="form-group">
-				    <label for="exampleFormControlSelect2">Select a previous analysis to veiw</label>
+
 				    <select multiple className="form-control" id="exampleFormControlSelect2">
-				      <Options />
+							{
+						this.state.analysis.map((a)=> {
+							return <Options analysis={a} key={a._id} />
+						})
+					}
 				    </select>
 				  </div>
 				<button className="btn btn-primary">View analysis</button>
