54922e863bc833b0c85fa7bcf8ac5ddf437d6576
Tom Becker <tjbecker@me.com>
2019-08-01 11:46:10 +0700

    style

--- src/topnav.js
+++ src/topnav.js
@@ -18,7 +18,7 @@ class Topnav extends Component {
 		return (
 			<nav id="navbar-top" className="navbar fixed-top navbar-light bg-light">
 			<span className="input-group-btn">
-				<Link to="/app" >Moodlysis</Link>
+				<Link id="home-topnav" to="/app" >Moodlysis</Link>
 			</span>
 			<span onClick={this.logout} className="input-group-btn">
 				<Link to="/login" >Logout</Link>
